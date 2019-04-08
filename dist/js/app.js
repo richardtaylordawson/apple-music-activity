d94cf4c57832a894abfd47b69e509d6ab3dcd5c1
Taylor Dawson <taylor.dawson@clearlink.com>
2019-04-08 15:50:08 -0600

    changing top scroll color

--- dist/js/app.js
+++ dist/js/app.js
@@ -396,7 +396,7 @@ class AppleMusicActivity {
   startDemo() {
     // Setting timeout to ensure transition is complete before getting data
     setTimeout(() => {
-      const url = "https://raw.githubusercontent.com/richardtaylordawson/apple-music-activity/master/assets/files/Apple%20Music%20Play%20Activity.csv";
+      const url = "https://raw.githubusercontent.com/richardtaylordawson/apple-music-activity/master/dist/files/Apple%20Music%20Play%20Activity.csv";
 
       fetch(url)
         .then((res) => res.text())
