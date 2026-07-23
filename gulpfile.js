c8c32db4fffa73d29cddf04d3e6dc3d38705c9b8
Taylor Dawson <richard.taylor.dawson@gmail.com>
2026-07-22 22:42:10 -0600

    Fix Vercel build for Node 24

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,40 @@
 const gulp = require("gulp")
-const sass = require("gulp-sass")
+const dartSass = require("sass")
+const gulpSass = require("gulp-sass")
 const browserSync = require("browser-sync").create()
-const imagemin = require("gulp-imagemin")
-const cache = require("gulp-cache")
 const del = require("del")
-const runSequence = require("run-sequence")
 const cleanCSS = require("gulp-clean-css")
 const uglify = require("gulp-uglify")
 const htmlmin = require("gulp-htmlmin")
 const rollup = require("gulp-better-rollup")
 const babel = require("rollup-plugin-babel")
 
-gulp.task("images", () => {
+const sass = gulpSass(dartSass)
+
+gulp.task("images", () =>
   gulp
     .src("_src/images/**/*.+(png|jpg|jpeg|gif|svg|ico)")
-    .pipe(cache(imagemin()))
     .pipe(gulp.dest("dist/images"))
+)
 
-  return del.sync("dist/images")
-})
-
-gulp.task("js", () => {
+gulp.task("statsJs", () =>
   gulp
-    .src("_src/js/stats.js")
+    .src("_src/js/stats.js", { allowEmpty: true })
     .pipe(rollup({ plugins: [babel()] }, { format: "cjs" }))
     .pipe(uglify())
     .pipe(gulp.dest("dist/js"))
+)
 
-  return gulp
+gulp.task("appJs", () =>
+  gulp
     .src("_src/js/app.js")
     .pipe(rollup({ plugins: [babel()] }, { format: "cjs" }))
     .pipe(uglify())
     .pipe(gulp.dest("dist/js"))
     .pipe(browserSync.reload({ stream: true }))
-})
+)
+
+gulp.task("js", gulp.series("statsJs", "appJs"))
 
 gulp.task("scss", () => {
   return gulp
@@ -44,21 +45,17 @@ gulp.task("scss", () => {
     .pipe(browserSync.reload({ stream: true }))
 })
 
-gulp.task("html", () => {
+gulp.task("html", () =>
   gulp
     .src("_src/**/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("dist/"))
     .pipe(browserSync.reload({ stream: true }))
+)
 
-  return del.sync("dist/**/*.html")
-})
-
-gulp.task("altFiles", () => {
+gulp.task("altFiles", () =>
   gulp.src("_src/**/*.+(json|txt|xml)").pipe(gulp.dest("dist/"))
-
-  return del.sync("dist/**/*.+(json|txt|xml)")
-})
+)
 
 gulp.task("fonts", function () {
   return gulp.src("_src/fonts/**/*").pipe(gulp.dest("dist/fonts"))
@@ -68,43 +65,38 @@ gulp.task("files", function () {
   return gulp.src("_src/files/**/*").pipe(gulp.dest("dist/files"))
 })
 
-gulp.task("syncDist", () => del.sync("dist"))
+gulp.task("syncDist", (done) => {
+  del.sync("dist")
+  done()
+})
 
 gulp.task("browserSync", () =>
   browserSync.init({ server: { baseDir: "./dist" } })
 )
 
 gulp.task("watch", () => {
-  gulp.watch("_src/images/**/*.+(png|jpg|jpeg|gif|svg)", ["images"])
-  gulp.watch("_src/js/**/*.js", ["js"])
-  gulp.watch("_src/scss/**/*.scss", ["scss"])
-  gulp.watch("_src/**/*.html", ["html"])
-  gulp.watch("_src/**/*.+(json|txt|xml)", ["altFiles"])
-  gulp.watch("_src/fonts/**/*", ["fonts"])
-  gulp.watch("_src/files/**/*", ["files"])
+  gulp.watch("_src/images/**/*.+(png|jpg|jpeg|gif|svg)", gulp.series("images"))
+  gulp.watch("_src/js/**/*.js", gulp.series("js"))
+  gulp.watch("_src/scss/**/*.scss", gulp.series("scss"))
+  gulp.watch("_src/**/*.html", gulp.series("html"))
+  gulp.watch("_src/**/*.+(json|txt|xml)", gulp.series("altFiles"))
+  gulp.watch("_src/fonts/**/*", gulp.series("fonts"))
+  gulp.watch("_src/files/**/*", gulp.series("files"))
 })
 
-gulp.task("build", (callback) => {
-  runSequence(
-    ["images", "js", "scss", "html", "altFiles", "fonts", "files"],
-    callback
-  )
-})
-
-gulp.task("default", (callback) => {
-  runSequence(
+gulp.task(
+  "build",
+  gulp.series(
     "syncDist",
-    [
-      "images",
-      "js",
-      "scss",
-      "html",
-      "altFiles",
-      "fonts",
-      "files",
-      "browserSync",
-      "watch",
-    ],
-    callback
+    gulp.parallel("images", "js", "scss", "html", "altFiles", "fonts", "files")
   )
-})
+)
+
+gulp.task(
+  "default",
+  gulp.series(
+    "syncDist",
+    gulp.parallel("images", "js", "scss", "html", "altFiles", "fonts", "files"),
+    gulp.parallel("browserSync", "watch")
+  )
+)
