2df679b749b6c614698e5a03dd8e598f51e9bd02
Taylor Dawson <richard.taylor.dawson@gmail.com>
2026-07-22 22:58:29 -0600

    Fix demo data URL

--- _src/js/app.js
+++ _src/js/app.js
@@ -476,8 +476,7 @@ class AppleMusicActivity {
   startDemo() {
     // Setting timeout to ensure transition is complete before getting data
     setTimeout(() => {
-      const url =
-        "https://raw.githubusercontent.com/richardtaylordawson/apple-music-activity/master/dist/files/Apple%20Music%20Play%20Activity.csv"
+      const url = "/files/Apple%20Music%20Play%20Activity.csv"
 
       fetch(url)
         .then((res) => res.text())
